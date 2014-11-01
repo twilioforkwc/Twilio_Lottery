f73aafddfcf98c94138a93a2522c3bccfd52e08d
八木都志郎 <toshiro@mba.local>
2014-11-01 23:00:48 +0900

    init

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ var express = require('express');
 var mongoose = require('mongoose'); 
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+//var busboy = require('connect-busboy');
+var multer  = require('multer');
+//var formidable = require('formidable');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var ECT = require('ect');
 var twilio = require('twilio');
 var csrf = require('csurf');
+var path = require('path');
+var fs = require('fs-extra');
 
 var app = express();
 
 // Cookie
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 // Session
@@ -31,6 +37,7 @@ app.use(express['static'](__dirname + '/public'));
 
 // Model
 var Lottery = require('models/lottery');
+var Phone = require('models/phone');
 
 // Logs
 if (app.get('env') == 'production') {
@@ -42,6 +49,12 @@ if (app.get('env') == 'production') {
 // MongoDB
 var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI;
 mongoose.connect(connectionString);
+
+// File upload
+//app.use(busboy());
+app.use(multer({
+  dest: "./public/files/"
+}));
 
 // Application
 //Auth TokenやAccoutn SIDを入力するトップページ
@@ -73,16 +86,7 @@ app.post('/start', function(req, res){
           //アップグレードアカウントなら認証情報をセッションに
           req.session.sid = sid;
           req.session.auth_token = auth_token;
-          //アップグレードアカウントなら利用可能な電話番号リストを作成
-          var option_data = [];
-          client.availablePhoneNumbers('JP').local.get({voiceEnabled: true}, function(err, numbers){
-            for(var key in numbers.available_phone_numbers){
-              var number = numbers.available_phone_numbers[key];
-              option_data.push('<option value="'+number.phone_number+'">' + number.phone_number + '</option>');
-            }
-            var options = option_data.join('');
-            res.render('start', {title: 'Twilio抽選アプリ', options: options, csrf: req.csrfToken()}); 
-          });
+          res.redirect('/start');
         }
       }
     });
@@ -92,12 +96,65 @@ app.post('/start', function(req, res){
   }
 });
 
+app.get('/start', function(req, res){
+  //アップグレードアカウントなら利用可能な電話番号リストを作成
+  var option_data = [];
+  var sid = req.session.sid;
+  var auth_token = req.session.auth_token;
+  var client = new twilio.RestClient(sid, auth_token);
+  client.availablePhoneNumbers('JP').local.get({voiceEnabled: true}, function(err, numbers){
+    for(var key in numbers.available_phone_numbers){
+      var number = numbers.available_phone_numbers[key];
+      option_data.push('<option value="'+number.phone_number+'">' + number.phone_number + '</option>');
+    }
+    var options = option_data.join('');
+    res.render('start', {title: 'Twilio抽選アプリ', options: options, csrf: req.csrfToken()}); 
+  });
+});
+
+function saveAndRedirect(req, res, sid, auth_token, number, generated_token, voice_text, file_path, mode){
+  var lottery = new Lottery();
+  lottery.sid = sid;
+  lottery.auth_token = auth_token;
+  lottery.createdAt = new Date();
+  lottery.phone_number = number;
+  lottery.token = generated_token;
+  lottery.voice_file = file_path;
+  lottery.voice_text = voice_text;
+  lottery.mode = mode;
+  lottery.save(function(err){
+console.log(mode);
+    switch(mode){
+      case "trial":
+        if(err){
+          res.json({success: false, message: 'データを保存できませんでした'});
+        }else{
+          res.json({success: true, message: number + 'に電話をかけてください'});
+        }
+        break;
+      default:
+        if(err){
+          req.session.message = "エラーが発生しました";
+          res.redirect('/error');
+        }else{
+          res.redirect('/l/' + generated_token);
+        }
+        break;
+    }
+  });
+}
+
 //選択された電話番号から電話番号別抽選ページを作成してリダイレクト
+//modeがtrialの場合はJSONを返す
 app.post('/number', function(req, res){
   var sid = req.session.sid;
   var auth_token = req.session.auth_token;
   var number = req.param('phone_number');
+  var voice_text = req.param('voice_text');
+  var voice_file = req.param('voice_file');
+  var mode = req.param('mode');
   var generated_token;
+
   function random(){
     return Math.random().toString(36).substr(2);
   }
@@ -106,27 +163,53 @@ app.post('/number', function(req, res){
     Lottery.where({token: generated_token}).count(function(err, count){
       if(!err && count > 0){
         generate();
+        return;
       }else{
         generated_token = token;
         //MongoDBに登録
-        var lottery = new Lottery();
-        lottery.sid = sid;
-        lottery.auth_token = auth_token;
-        lottery.createdAt = new Date();
-        lottery.phone_number = number;
-        lottery.token = generated_token;
-        lottery.save(function(err){
-          if(err){
-            req.session.message = "エラーが発生しました";
-            res.redirect('/error');
-          }else{
-            res.redirect('/l/' + generated_token);
-          }
-        });
+        var save_path = "";
+        save_path = __dirname + "files";
+        if(req.files.voice_file){
+          fs.exists(req.files.voice_file.path, function(exists){
+            if(exists){
+              saveAndRedirect(req, res, sid, auth_token, number, generated_token, voice_text, req.files.voice_file.path, mode);
+            }else{
+              saveAndRedirect(req, res, sid, auth_token, number, generated_token, voice_text, req.files.voice_file.path, mode);
+            }
+          });
+        }else{
+            saveAndRedirect(req, res, sid, auth_token, number, generated_token, voice_text, null, mode);
+        }
       }
     });
   }
-  generate();
+  //同じ電話番号が登録されていたらTrialなら消して本番ならエラー
+  Lottery.find({phone_number: number}, function(err, docs){
+    var data =[];
+    if(docs.length > 0){
+      for(var i = 0, len = docs.length; i < len; i++){
+        if(docs[i].mode === "trial"){
+          docs[i].remove();
+        }else{
+          data.push(docs[i]);
+        }
+      }
+      if(data.length > 0){
+        //同じ番号で本番データがある
+        res.json({error: true, message: "この番号は現在既に利用されています"});
+      }else{
+        generate();
+      }
+    }else{
+      generate();
+    }
+  });
+});
+
+app.get('/token', function(req, res){
+  if(req.xhr){
+    res.json({csrf: req.csrfToken()});  
+  }
 });
 
 //電話番号別抽選ページ
@@ -142,8 +225,114 @@ app.get('/l/:token', function(req, res){
     }else{
       message = "";
     }
-    res.render('lottery', {title: 'Twilio抽選アプリ' + req.params.token, message: message});  
+    getCandidateCount({token: docs[0].token}, function(num){
+      res.render('lottery', {title: 'Twilio抽選アプリ', message: message, num: num, token: docs[0].token, csrf: req.csrfToken()});  
+    });
   });
+});
+
+function getCandidateCount(args, callback){
+  var num = 0;
+  Phone.where(args).count(function(err, count){
+    if(count){
+      num = count;
+    }
+    callback(num);
+  });
+}
+function getCandidates(args, callback){
+  Phone.find(args, callback);
+}
+
+// 応募者数
+app.get('/candidates', function(req, res){
+  getCandidateCount({token: req.param('token')}, function(num){
+    res.json({num: num});
+  });
+});
+//当選者数
+app.get('/winners', function(req, res){
+  getCandidateCount({status: 'win', token: req.param('token')}, function(num){
+    res.json({num: num});
+  });
+});
+
+function shuffle(array) {
+  var counter = array.length, temp, index;
+  while (counter > 0) {
+      index = Math.floor(Math.random() * counter);
+      counter--;
+      temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+  }
+  return array;
+}
+//当選実行
+app.post('/l/:token', function(req, res){
+  Lottery.find({token: req.param('token')}, function(err, lotteries){
+    if(!err && lotteries[0]){
+      var num = parseInt(req.param('num'), 10);
+      if(num <= 0){
+        res.json({success: false, message: '当選者数を1以上の数値で指定してください'});  
+      }else{
+        var args = {token: req.param('token')};
+        if(req.param('no_dup')){
+          args.status = null;
+        }
+        Phone.find(args, function(err, docs){
+          if(err){
+            res.json({success: false, message: "データベースにエラーが発生しました"});
+          }else{
+            if(docs.length <= 0){
+              res.json({success: false, message: "応募者が見つかりませんでした"});
+            }else if(docs.length < num){
+              res.json({success: false, message: "応募者数が当選者数より少ないため実行できません"});
+            }else{
+              // 当選処理開始
+              lotteries[0].status = 'calling';
+              lotteries[0].save(function(e){
+                if(e){
+console.log(e);
+                }else{
+                  var data = shuffle(docs);
+                  for(var i = 0, len = data.length; i < len; i++){
+                    data[i].status = 'calling';
+                    phoneCall({data: data[i], lottery: lotteries[0]});
+                  }
+                }
+              });
+            }
+          }
+        });
+      }
+    }else{
+      res.json({success: false, message: '抽選は終了しています'});
+    }
+  });
+});
+
+// 当選者に電話をかける
+function phoneCall(args){
+  var client = new twilio.RestClient(args.lottery.account_sid, args.lottery.auth_token);
+  client.makeCall({
+    to: args.data.phone_number,
+    from: args.lottery.phone_number,
+    url: '/call/' + args.lottery.token
+  }, function(err, call){
+    if(err){
+      args.data.status = 'error';
+    }else{
+      args.data.status = 'won';
+    }
+    args.save();
+  });
+}
+
+// 終了
+app.post('/destroy/:token', function(req, res){
+  // 抽選データ削除
+  res.json({success: true});
 });
 
 //Twilioからのリクエストかチェック
@@ -161,12 +350,70 @@ function validateToken(sid, token, callback){
     }
   });
 }
+
+//Twilioでエラーメッセージを話す
+function speakErrorMessage(res, message){
+  var resp = new twilio.TwimlResponse();
+  res.xml(resp.say(message, {language: 'ja-jp'}));
+}
+//着電するとTwilioから呼び出される
+app.post('/call/:token', function(req, res){
+  var resp = new twilio.TwimlResponse();
+  validateToken(req.param('AccountSid'), req.param('token'), function(e){
+    Lottery.find({token: req.param('token')}, function(err, docs){
+      if(err){
+        speakErrorMessage(res, "エラーが発生しました。通話を終了します");
+      }else{
+        var l = docs[0];
+        if(l.voice_file){
+          res.xml(resp.play("/" + l.voice_file));
+        }else{
+          res.xml(resp.say(l.voice_text));
+        }
+      }
+    });
+  });
+});
+
 //着電するとTwilioから呼び出される
 app.post('/twiml/:token', function(req, res){
   validateToken(req.param('AccountSid'), req.params.token, function(e){
-
+    //Toからアプリケーションとユーザを検索
+    Lottery.find({phone_number: req.param('To')}, function(err, docs){
+      if(err || docs.length <= 0){
+        //見つからなかったらエラー処理
+        speakErrorMessage(res, 'おかけになった電話番号は既に抽選が終了しているか、登録されていないためご利用できません');
+        console.log(docs);
+      }else{
+        //見つかったら通話履歴チェック
+        Phone.find({phone_number: req.param('To')}, function(err, p_docs){
+          if(err || p_docs.length <= 0){
+            //履歴が見つからなければ履歴保存
+            console.log(p_docs);
+            var phone = new Phone();
+            phone.phone_number = req.param('To');
+            phone.token = docs[0].token;
+            phone.save(function(e){
+              if(e){
+                //error
+                speakErrorMessage(res, '申し訳ございません。エラーが発生しました');
+              }else{
+                //指定された方法で返信を開始
+console.log('返信開始');
+              }
+            });
+          }else{
+            //２回目ならキャンセル処理（過去の履歴は削除）
+            console.log(p_docs);
+            for(var k = 0, l = p_docs.length; k < l; k++){
+              p_docs[k].remove();
+            }
+          }
+        });
+      }
+    });
   }, function(e){
-
+    speakErrorMessage(res, 'エラーが発生しました');
   });
 });
 //通話がエラーになった
---
+++ public/javascripts/all.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+  function updateToken(callback){
+    $.ajax({
+      url: '/token',
+      method: 'GET',
+      success: function(e){
+        $('#csrf').val(e.csrf);
+        callback();
+      }
+    });
+  }
+  $('#tmp_button').click(function(e){
+    updateToken(function(e){
+      var data = $('#the_form').serialize();
+      data += "&mode=trial";
+      if($('#voice_file').val()){
+        data += "&" + encodeURIComponent($('#voice_file').val());
+      }
+      $.ajax({
+        url: '/number',
+        method: 'POST',
+        data: data,
+        success: function(e){alert("success:" + e.message);},
+        error: function(e){alert('エラーが発生しました');}
+      });
+    });
+    return false;
+  });
+  if($('#candidates')){
+    setInterval(function(e){
+      $.ajax({
+        url: '/candidates?id=' + $('#token').html(),
+        method: 'GET',
+        success: function(e){
+          $('#candidates').html(e.num);
+        }
+      });
+      $.ajax({
+        url: '/winners?id=' + $('#token').html(),
+        method: 'GET',
+        success: function(e){
+          $('#winners').html(e.num);
+        }
+      });
+    }, 3000);
+  }
+
+  $('#select_winners_button').click(function(){
+    var data = $('#select_winners').serialize();
+    $.ajax({
+      url: '/l/' + $('#token').html() + "?csrf=" + $('#csrf').val(),
+      method: 'POST',
+      data: data,
+      success: function(e){
+        if(e.success === false){
+          alert(e.message);
+        }
+      }
+    });
+    return false;
+  });
+
+  $('#destroy').click(function(){
+    if(confirm("抽選を終了しますか？この操作は取り消しできません")){
+      console.log('destroy');  
+      $.ajax({
+        url: '/destroy/' + $('#token').html() + '?_csrf=' + $('#csrf').val(),
+        method: 'POST',
+        success: function(e){
+          window.location.href="/";
+        }
+      });
+    }
+  });
+
+});
