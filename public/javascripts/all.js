ec270e3865d14761d1d0649d3a93212ddda3ada7
八木都志郎 <toshiro@mba.local>
2014-11-03 13:17:24 +0900

    add xml response

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -74,7 +74,7 @@ $(document).ready(function(){
   var winner_timer;
 
   $('#select_winners_button').click(function(){
-    updateToken(startSelection());
+    updateToken(startSelection);
     return false;
   });
 
