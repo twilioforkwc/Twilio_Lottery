2aaa6289cbbb158a352714e5bc5615f511cc302a
八木都志郎 <toshiro@mba.local>
2014-11-18 01:12:00 +0900

    test for azure

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -137,7 +137,6 @@ $(document).ready(function(){
               }
             $('#table').append('<tr><th>'+e.data[i].phone_number.substr(-4)+'</th><td><ul><li class="'+className+'">'+status+ postfix + '</ul></td></tr>');
           } 
-alert(finished);
           $('#finished').html(finished);
         }
       }
