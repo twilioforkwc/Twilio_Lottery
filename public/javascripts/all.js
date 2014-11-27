06ce82034b57145bd0d867df11cafa8666f7c2f2
八木都志郎 <toshiro@mba.local>
2014-11-27 13:08:50 +0900

    remove timeout

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -13,12 +13,10 @@ $(document).ready(function(){
   }
 
   function hideMovie(){
-    setTimeout(function(){
-      $('.loading-movie').effect('fade', {}, 500, function(){
-        $('.loading-dark').remove();
-        $('.loading-movie').remove();
-      });
-    }, 1000);
+    $('.loading-movie').effect('fade', {}, 500, function(){
+      $('.loading-dark').remove();
+      $('.loading-movie').remove();
+    });
   }
 
   $('#login_button').click(function(e){
