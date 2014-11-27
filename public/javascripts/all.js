c42864f6ee3bcb16189797b6dae0fc0c08ac47a0
八木都志郎 <toshiro@mba.local>
2014-11-27 13:04:47 +0900

    add callback

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -14,10 +14,11 @@ $(document).ready(function(){
 
   function hideMovie(){
     setTimeout(function(){
-      $('.loading-dark').remove();
-      $('.loading-movie').effect('fade', {}, 500);
-      $('.loading-movie').remove();
-    }, 3000);
+      $('.loading-movie').effect('fade', {}, 500, function(){
+        $('.loading-dark').remove();
+        $('.loading-movie').remove();
+      });
+    }, 2000);
   }
 
   $('#login_button').click(function(e){
