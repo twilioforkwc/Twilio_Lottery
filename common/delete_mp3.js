e55a3276ac528124f447e8f2ab67d56d3c91c5ae
八木都志郎 <toshiro@mba.local>
2014-11-27 13:22:05 +0900

    delete unused mp3

--- common/delete_mp3.js
+++ common/delete_mp3.js
@@ -2,6 +2,8 @@ var path = require('path');
 var fs = require('fs-extra');
 module.exports = function(doc){
   if(doc.voice_file){
-    console.log(doc.voice_file); 
+    fs.unlink(doc.voice_file, function(e){
+      console.log(e);
+    });
   }  
 };
