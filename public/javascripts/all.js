b6b2fb8e5434dce0a203d0ff2b4ca41d5fd5f90b
八木都志郎 <toshiro@mba.local>
2014-11-30 00:07:16 +0900

    update date

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -127,7 +127,7 @@ $(document).ready(function(){
         if(e.lottery){
           var createdAt = e.lottery.createdAt.toString().replace(/T/, ' ').replace(/\.[0-9]*Z/, '');
           var limit = Date.parseExact(createdAt, "yyyy-MM-dd HH:mm:ss").addHours(2).addMinutes(-15);
-          if(time_alert && limit < Date.today()){
+          if(time_alert && (limit > Date.today())){
             alert("あと15分でデータが消去されます。抽選を行って下さい。");
             time_alert = false;
           }
