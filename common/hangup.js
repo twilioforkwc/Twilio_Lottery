7dca9e510e9c359ba42d5a26225aeab71c332c71
八木都志郎 <toshiro@mba.local>
2014-11-29 15:51:43 +0900

    to string

--- common/hangup.js
+++ common/hangup.js
@@ -2,5 +2,5 @@ var twilio = require('twilio');
 module.exports = function(res){
   var resp = new twilio.TwimlResponse();
   res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(resp.hangup().toString());
+  res.end('<Response><Hangup/></Response>');
 };
