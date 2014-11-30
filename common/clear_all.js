ea7e612af925dff9300fc83d60762777642233fc
八木都志郎 <toshiro@mba.local>
2014-11-30 11:06:31 +0900

    clear call status

--- common/clear_all.js
+++ common/clear_all.js
@@ -1,6 +1,7 @@
 module.exports = function(docs, callback){
   for(var i = 0, l = docs.length; i < l; i++){
     docs[i].status = "";
+    docs[i].callstatus = "";
     docs[i].save();
   }
   callback();
