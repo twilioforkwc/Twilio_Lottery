877b5d6196094cd7c41bc5e24306dd9afa54e8c7
八木都志郎 <toshiro@mba.local>
2014-12-01 00:40:47 +0900

    add method

--- common/start_ivr.js
+++ common/start_ivr.js
@@ -3,9 +3,10 @@ module.exports = function(res, token){
   var resp = new twilio.TwimlResponse();
   res.writeHead(200, {'Content-Type': 'text/xml'});
   var xml = resp.gather({
-    action: '/twilio/cancel/' + token
+    action: '/twilio/cancel/' + token,
+    method: 'POST'
   }, function(node){
-    node.say('抽選の応募をキャンセルされる方は番号の１を押してください。', {language: 'ja-jp', loop: 3});
+    node.say('抽選の応募をキャンセルされる場合は番号の１を押してください。', {language: 'ja-jp', loop: 3});
   }).toString();
   res.end(xml);
 };
